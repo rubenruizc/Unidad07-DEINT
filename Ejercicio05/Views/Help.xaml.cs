de2a4c30737889410e10d825cb8f8be2732622f3
rruiz <rruiz@107-28>
2024-10-14 14:37:31 +0200

    Ejercicio 5 Unidad 7

---
+++ Ejercicio05/Views/Help.xaml.cs
@@ -0,0 +1,9 @@
+namespace Ejercicio05.Views;
+
+public partial class Help : ContentPage
+{
+	public Help()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Ejercicio05/Views/Settings.xaml.cs
@@ -0,0 +1,9 @@
+namespace Ejercicio05.Views;
+
+public partial class Settings : ContentPage
+{
+	public Settings()
+	{
+		InitializeComponent();
+	}
+}
